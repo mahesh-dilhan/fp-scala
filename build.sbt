1125527b48c05fac5faa4aa33f03a881b8d89f22
mahesh <mail.mahesh.lk@gmail.com>
2021-06-24 16:09:22 +0800

    fp-scalaz

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "filereader"
 version := "0.1"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies ++= Seq(
+  "org.scalaz" % "scalaz-core_2.12" % "7.2.9",
+)
--- src/main/scala/FileData.scala
+++ src/main/scala/FileData.scala
@@ -1,3 +1,5 @@
+import scalaz._
+
 object FileData {
 
   def main(args: Array[String]): Unit = {
@@ -14,9 +16,19 @@ object RawData {
   ).toSeq
 }
 
+case class Domainuser (person: Person, phonenumber: Phonenumber)
+
 case class Phonenumber (countryCode: Int, areaCode: Int,prefix:Int, lineNumber: Int )
+
+case class Results(successes: Int, failuers: Int)
+case class TransformError(error: String)
+
 object Phonenumber {
   private val pattern = """(\d{1})-(\d{3})-(\d{3})-(\d{4})""".r
+
+  def from(phonenumber: String) : TransformError \/ Phonenumber ={
+
+  }
 }
 
 case class Person (firstname: String, lastname: String)
